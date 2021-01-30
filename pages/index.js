4490fb96927ffac12431cf93ffff14bbe5df8176
Maciel-coder <macielrodrigues940@gmail.com>
2021-01-30 01:53:32 -0300

    refatorando

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import styled from 'styled-components'
 import dbJson from '../db.json';
+import QuizBackground from '../src/conponents/QuizBackground';
+import Widget from '../src/conponents/Widget';
+import QuizLogo from '../src/conponents/QuizLogo';
+import Footer from '../src/conponents/Footer';
+import GitHubCorner from '../src/conponents/GitHubCourner';
 
-const BackgroundImage = styled.div`
-  background-image: url(${dbJson.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
 
-`;
-
- const QuizContainer = styled.div`
+export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
   margin: auto 10%;
-  background-color: red;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
@@ -22,14 +19,31 @@ const BackgroundImage = styled.div`
 `;
 
 
+
   export default function Home() {
   return (
-    
-    <BackgroundImage>
-      <QuizContainer>
-        dksdjkjçdsko
-      </QuizContainer>
-    </BackgroundImage>
-    
-  )
+    <QuizBackground backgroundImage={dbJson.bg}>
+    <QuizContainer>
+      <QuizLogo />
+      <Widget>
+        <Widget.Header>
+          <h1>{dbJson.title}</h1>
+        </Widget.Header>
+        <Widget.Content>
+          <p>{dbJson.description}</p>
+        </Widget.Content>
+      </Widget>
+
+      <Widget>
+        <Widget.Content>
+          <h1>Quizes da Galera</h1>
+
+          <p>lorem ipsum dolor sit amet...</p>
+        </Widget.Content>
+      </Widget>
+      <Footer />
+    </QuizContainer>
+    <GitHubCorner projectUrl="https://github.com/omariosouto" />
+  </QuizBackground>
+);
 }
